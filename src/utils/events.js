f0319a5041d2bdab74133316bf824cdc8841a9aa
Jonasgg99 <jonaseg@gmail.com>
2026-01-09 20:10:08 +0100

    Revert "Added locate me button to  map"
    This reverts commit eb99274f9f4ea1a2ef3c189c9e735917ba2dce9c.

--- src/utils/events.js
+++ src/utils/events.js
@@ -7,8 +7,6 @@
 import { state } from '../services/state.js';
 import { close as closeDrawer } from '../components/Drawer.js';
 import { collapse as collapseSheet } from '../components/BottomSheet.js';
-import { centerOnUser } from '../components/Map.js';
-import { showToast } from './toast.js';
 
 
 /**
@@ -26,21 +24,6 @@ export function setupEventListeners() {
   
   // Prevent pull-to-refresh on mobile
   document.body.addEventListener('touchmove', preventOverscroll, { passive: false });
-  
-  // Locate me button
-  document.getElementById('locateBtn').addEventListener('click', handleLocateClick);
-}
-
-
-/**
- * Handle locate button click
- */
-function handleLocateClick() {
-  if (state.gps.currentPosition) {
-    centerOnUser();
-  } else {
-    showToast('Waiting for GPS...', 'error');
-  }
 }
 
 
